a2807809c8fa1c1b860d66b437900b8b9ebc0a0b
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-09 18:40:44 -0400

    Comment out the original palindrome check implementation and retain the alternative solution

--- _2_palindromeNumber.ts
+++ _2_palindromeNumber.ts
@@ -1,23 +1,29 @@
-function reverseNumber(num: number): number {
-    let reversed = 0;
-    while (num !== 0) {
-        const digit = num % 10;
-        reversed = reversed * 10 + digit;
-        num = Math.floor(num / 10);
-    }
-    return reversed;
-}
+// My solution
+// function reverseNumber(num: number): number {
+//     let reversed = 0;
+//     while (num !== 0) {
+//         const digit = num % 10;
+//         reversed = reversed * 10 + digit;
+//         num = Math.floor(num / 10);
+//     }
+//     return reversed;
+// }
 
-function isPalindrome(x: number): boolean {
-    if (x < 0) {
-        return false;
-    }
+// function isPalindrome(x: number): boolean {
+//     if (x < 0) {
+//         return false;
+//     }
     
-    const reversed = reverseNumber(x);
-    return reversed === x;
-}
+//     const reversed = reverseNumber(x);
+//     return reversed === x;
+// }
 
-// Pruebas (esto no va en LeetCode, solo para probar localmente)
-console.log("Result:", isPalindrome(1001)); // true
-console.log("Result:", isPalindrome(12321)); // true
-console.log("Result:", isPalindrome(12345)); // false
+
+//other solution
+function isPalindrome(x: number): boolean {
+    return String(x) == String(x).split('').reverse().join('')
+};
+
+console.log("Result:", isPalindrome(1001)); 
+console.log("Result:", isPalindrome(12321)); 
+console.log("Result:", isPalindrome(12345));
