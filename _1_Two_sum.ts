8b085fa9ed7354bb783288504fbd26e62f5fe76d
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-09 18:47:41 -0400

    Add implementation of Two Sum algorithm with test cases

---
+++ _1_Two_sum.ts
@@ -0,0 +1,18 @@
+function twoSum(nums: number[], target: number): number[] {
+    const numMap = new Map<number, number>();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement)!, i];
+        }
+        numMap.set(nums[i], i);
+    }
+    
+    return []; 
+}
+
+// Pruebas
+console.log(twoSum([2, 7, 11, 15], 9));  // [0, 1]
+console.log(twoSum([3, 2, 4], 6));       // [1, 2]
+console.log(twoSum([3, 3], 6));          // [0, 1]
