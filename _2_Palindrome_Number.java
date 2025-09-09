f49f67416cfaa52b740f3a04a3043bc0d67f3340
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-09 16:51:20 -0400

    Refactor palindrome check implementation and update test case

--- _2_Palindrome_Number.java
+++ _2_Palindrome_Number.java
@@ -3,15 +3,30 @@
 public class _2_Palindrome_Number {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        boolean result = sol.isPalindrome(10);
+        boolean result = sol.isPalindrome(0);
         System.out.println("Result: " + result);
     }
 }
-
+// My solution
 class Solution {
     public boolean isPalindrome(int x) {
-        int amountDigits = String.valueOf(Math.abs(x)).length();
-        System.out.println(amountDigits);
-        return true;
+
+        if ( x < 0) {
+            return false;
+        }
+        
+        int reversed = reverseNumber(x);
+
+        return reversed == x;
+    }
+
+    public int reverseNumber(int num) {
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;        
+            reversed = reversed * 10 + digit;  
+            num = num / 10;             
+        }
+        return reversed;
     }
 }
