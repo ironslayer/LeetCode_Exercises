973902a72a552b3d633698a4dfd5149b0e45b0b8
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-10 15:07:10 -0400

    Other solution

--- _3_Roman_to_Integer.java
+++ _3_Roman_to_Integer.java
@@ -113,24 +113,57 @@ public class _3_Roman_to_Integer {
 //         }
 // }
 
+//other solution
+// class Solution {
+//     public int romanToInt(String s) {
+//         int[] values = new int[256]; 
+//         values['I'] = 1;
+//         values['V'] = 5;
+//         values['X'] = 10;
+//         values['L'] = 50;
+//         values['C'] = 100;
+//         values['D'] = 500;
+//         values['M'] = 1000;
+
+//         int total = 0;
+//         for (int i = 0; i < s.length() - 1; i++) {
+//             int current = values[s.charAt(i)];
+//             int next = values[s.charAt(i+1)];
+//             total += current < next ? -current : current;
+//         }
+//         return total + values[s.charAt(s.length() - 1)];
+//     }
+// }
+
+//other solution
 
 class Solution {
-    public int romanToInt(String s) {
-        int[] values = new int[256]; 
-        values['I'] = 1;
-        values['V'] = 5;
-        values['X'] = 10;
-        values['L'] = 50;
-        values['C'] = 100;
-        values['D'] = 500;
-        values['M'] = 1000;
-
-        int total = 0;
-        for (int i = 0; i < s.length() - 1; i++) {
-            int current = values[s.charAt(i)];
-            int next = values[s.charAt(i+1)];
-            total += current < next ? -current : current;
+    private int valueOf(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
         }
-        return total + values[s.charAt(s.length() - 1)];
+    }
+
+    public int romanToInt(String s) {
+        int result = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = valueOf(s.charAt(i));
+            if (curr < prev) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+            prev = curr;
+        }
+        return result;
     }
 }
