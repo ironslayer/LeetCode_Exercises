778fa843ec1bfe0430d5ce62e74ac4c0148389ba
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-12 20:42:15 -0400

    Other solution 5 in typescript

--- _5_Valid_Parentheses.ts
+++ _5_Valid_Parentheses.ts
@@ -1,32 +1,53 @@
 //Solution
+// function isValid(s: string): boolean {
+//     if (s.length % 2 !== 0) return false;
+
+//     const chr: string[] = s.split('');
+
+//     const stack: string[] = new Array(s.length);
+//     let top = 0;
+
+//     for (let i = 0; i < chr.length; i++) {
+//         switch (chr[i]) {
+//             case '(': case '{': case '[':
+//                 stack[top++] = chr[i];  
+//                 break;
+//             case ')':
+//                 if (top === 0 || stack[--top] !== '(') return false; 
+//                 break;
+//             case '}':
+//                 if (top === 0 || stack[--top] !== '{') return false;
+//                 break;
+//             case ']':
+//                 if (top === 0 || stack[--top] !== '[') return false;
+//                 break;
+//         }
+//     }
+
+//     return top === 0; 
+// }
+
+//other solution
+
 function isValid(s: string): boolean {
-    if (s.length % 2 !== 0) return false;
+    const bracketMap: { [key: string]: string } = {
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    };
 
-    const chr: string[] = s.split('');
-
-    const stack: string[] = new Array(s.length);
-    let top = 0;
-
-    for (let i = 0; i < chr.length; i++) {
-        switch (chr[i]) {
-            case '(': case '{': case '[':
-                stack[top++] = chr[i];  
-                break;
-            case ')':
-                if (top === 0 || stack[--top] !== '(') return false; 
-                break;
-            case '}':
-                if (top === 0 || stack[--top] !== '{') return false;
-                break;
-            case ']':
-                if (top === 0 || stack[--top] !== '[') return false;
-                break;
+    const stack: string[] = [];
+    for (const c of s) {
+        if ('([{'.includes(c)) {
+            stack.push(c);
+        } else {
+            if (bracketMap[c] !== stack.pop())
+                return false;
         }
     }
 
-    return top === 0; 
+    return stack.length === 0;
 }
-
 
 console.log(isValid("()[]{}"));  // true
 console.log(isValid("(){}}{"));  // false
