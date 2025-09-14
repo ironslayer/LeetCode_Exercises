d39f80df28bac04671adfc687ef2ed4f2ce54ef0
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-13 23:50:07 -0400

    chore exercise 6

--- _6_Merge_Two_Sorted_Lists.java
+++ _6_Merge_Two_Sorted_Lists.java
@@ -35,52 +35,52 @@ class ListNode {
   }
 
 // My solution
-// class Solution {
-//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-//         ListNode dummy = new ListNode(0);
-//         ListNode current = dummy;
-//         while (list1 != null && list2 != null) {
-//             if (list1.val <= list2.val) {
-//                 current.next = list1;    
-//                 list1 = list1.next;      
-//             } else {
-//                 current.next = list2;     
-//                 list2 = list2.next;       
-//             }
-//             current = current.next;      
-//         }
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;    
+                list1 = list1.next;      
+            } else {
+                current.next = list2;     
+                list2 = list2.next;       
+            }
+            current = current.next;      
+        }
 
-//         if (list1 != null) current.next = list1;
-//         if (list2 != null) current.next = list2;
+        if (list1 != null) current.next = list1;
+        if (list2 != null) current.next = list2;
 
-//         return dummy.next; 
-//     }
-// }
+        return dummy.next; 
+    }
+}
 
 
 // other solution
 
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         if (list1 == null) return list2;
+//         if (list2 == null) return list1;
 
-        ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
+//         ListNode dummy = new ListNode(-1);
+//         ListNode tail = dummy;
 
-        ListNode p1 = list1, p2 = list2;
-        while (p1 != null && p2 != null) {
-            if (p1.val <= p2.val) {
-                tail.next = p1;
-                p1 = p1.next;
-            } else {
-                tail.next = p2;
-                p2 = p2.next;
-            }
-            tail = tail.next;
-        }
+//         ListNode p1 = list1, p2 = list2;
+//         while (p1 != null && p2 != null) {
+//             if (p1.val <= p2.val) {
+//                 tail.next = p1;
+//                 p1 = p1.next;
+//             } else {
+//                 tail.next = p2;
+//                 p2 = p2.next;
+//             }
+//             tail = tail.next;
+//         }
 
-        tail.next = (p1 != null) ? p1 : p2;
-        return dummy.next;
-    }
-}
+//         tail.next = (p1 != null) ? p1 : p2;
+//         return dummy.next;
+//     }
+// }
