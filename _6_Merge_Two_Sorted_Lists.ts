98a6517e48b4be83d5cfe68d50a5354cd91d1ac7
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-13 23:56:07 -0400

    Other solution in typeescript at problem 6

--- _6_Merge_Two_Sorted_Lists.ts
+++ _6_Merge_Two_Sorted_Lists.ts
@@ -8,26 +8,63 @@ class ListNode {
     }
 }
 // My solution
-function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    const dummy = new ListNode(0);
-    let current = dummy;
+// function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+//     const dummy = new ListNode(0);
+//     let current = dummy;
 
-    while (list1 !== null && list2 !== null) {
-        if (list1.val <= list2.val) {
-            current.next = list1;
-            list1 = list1.next;
-        } else {
-            current.next = list2;
-            list2 = list2.next;
+//     while (list1 !== null && list2 !== null) {
+//         if (list1.val <= list2.val) {
+//             current.next = list1;
+//             list1 = list1.next;
+//         } else {
+//             current.next = list2;
+//             list2 = list2.next;
+//         }
+//         current = current.next;
+//     }
+
+//     if (list1 !== null) current.next = list1;
+//     if (list2 !== null) current.next = list2;
+
+//     return dummy.next; 
+// }
+
+//other solution
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+    let newList = list1.val < list2.val ? list1 : list2;
+    let curList1 = list1.val < list2.val ? list1.next : list1;
+    let curList2 = list1.val < list2.val ? list2 : list2.next;
+    let newListAnchor = newList;
+    while (curList1 != null || curList2 != null) {
+        if (curList1 == null) {
+            newList.next = curList2;
+            curList2 = curList2?.next ?? null;
+            newList = newList.next!;
+            continue;
         }
-        current = current.next;
+        if (curList2 == null) {
+            newList.next = curList1;
+            curList1 = curList1.next;
+            newList = newList.next;
+            continue;
+        }
+
+        if(curList1.val < curList2.val) {
+            newList.next = curList1;
+            curList1 = curList1.next;
+        }
+        else {
+            newList.next = curList2;
+            curList2 = curList2.next;
+        }
+
+        newList = newList.next;
     }
 
-    if (list1 !== null) current.next = list1;
-    if (list2 !== null) current.next = list2;
-
-    return dummy.next; 
-}
+    return newListAnchor;
+};
 
 function printList(head: ListNode | null) {
     let cur = head;
