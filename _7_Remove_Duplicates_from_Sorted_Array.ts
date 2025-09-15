02956fbe913f480019fdf75aff94f00403d2cffa
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-14 20:45:04 -0400

    Other solution problmen 7

--- _7_Remove_Duplicates_from_Sorted_Array.ts
+++ _7_Remove_Duplicates_from_Sorted_Array.ts
@@ -1,17 +1,33 @@
 // My solution
-function removeDuplicates(nums: number[]): number {
-    if (nums.length === 0) return 0;
+// function removeDuplicates(nums: number[]): number {
+//     if (nums.length === 0) return 0;
 
-    let j = 1; 
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[i - 1]) {
-            nums[j] = nums[i];
-            j++;
+//     let j = 1; 
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i] !== nums[i - 1]) {
+//             nums[j] = nums[i];
+//             j++;
+//         }
+//     }
+
+//     return j;
+// }
+
+//Other solution
+function removeDuplicates(nums: number[]): number {
+    let k = 1
+
+    for(let i = 1; i< nums.length; i++){
+        if(nums[i] != nums[k -1]){
+            nums[k] = nums[i]
+            k ++;
         }
     }
-
-    return j;
-}
+    for(let i = k; i< nums.length; i++){
+        (nums as any)[i] = "_"
+    }
+        return k;
+};
 
 
 const nums1 = [0,0,1,1,1,2,2,3,3,4];
