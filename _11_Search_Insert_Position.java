7923b080c01f2ed55c1e561e9fa5551181c850ff
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-18 23:06:14 -0400

    Solution problem 11 in java

---
+++ _11_Search_Insert_Position.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class _11_Search_Insert_Position {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {1, 3, 5, 6};
+        int target1 = 5;
+        System.out.println("Input: " + Arrays.toString(nums1) + ", target=" + target1);
+        System.out.println("Output: " + sol.searchInsert(nums1, target1)); 
+
+        int[] nums2 = {1, 3, 5, 6};
+        int target2 = 2;
+        System.out.println("Input: " + Arrays.toString(nums2) + ", target=" + target2);
+        System.out.println("Output: " + sol.searchInsert(nums2, target2)); 
+
+        int[] nums3 = {1, 3, 5, 6};
+        int target3 = 7;
+        System.out.println("Input: " + Arrays.toString(nums3) + ", target=" + target3);
+        System.out.println("Output: " + sol.searchInsert(nums3, target3)); 
+
+        int[] nums4 = {1, 3, 5, 6};
+        int target4 = 0;
+        System.out.println("Input: " + Arrays.toString(nums4) + ", target=" + target4);
+        System.out.println("Output: " + sol.searchInsert(nums4, target4)); 
+    }
+}
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
