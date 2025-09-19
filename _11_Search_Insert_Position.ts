e432a326d3397ef03a1072ed97b99cf4ba0fb3fd
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-18 23:11:27 -0400

    Other solution in typescript problme 11

--- _11_Search_Insert_Position.ts
+++ _11_Search_Insert_Position.ts
@@ -1,22 +1,34 @@
+// function searchInsert(nums: number[], target: number): number {
+//     let start = 0;
+//     let end = nums.length - 1;
+
+//     while (start <= end) {
+//         const mid = start + ((end - start) >>> 1);
+
+//         if (target === nums[mid]) {
+//             return mid;
+//         }
+//         if (target > nums[mid]) {
+//             start = mid + 1;
+//         } else {
+//             end = mid - 1;
+//         }
+//     }
+
+//     return start;
+// }
+
+
+//Other solution
 function searchInsert(nums: number[], target: number): number {
-    let start = 0;
-    let end = nums.length - 1;
-
-    while (start <= end) {
-        const mid = start + ((end - start) >>> 1);
-
-        if (target === nums[mid]) {
-            return mid;
-        }
-        if (target > nums[mid]) {
-            start = mid + 1;
-        } else {
-            end = mid - 1;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]>=target){
+            return i;
         }
     }
 
-    return start;
-}
+    return nums.length
+};
 
 console.log(searchInsert([1, 3, 5, 6], 5)); 
 console.log(searchInsert([1, 3, 5, 6], 2)); 
