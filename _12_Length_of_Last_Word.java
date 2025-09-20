e2a2d3a39fe091dd24e05b5bbf7b8781c863b992
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-19 23:34:22 -0400

    Other solution in java problem 12

--- _12_Length_of_Last_Word.java
+++ _12_Length_of_Last_Word.java
@@ -21,9 +21,28 @@ public class _12_Length_of_Last_Word {
 
 //Other solution
 
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] st = s.split(" ");
+//         return st[st.length-1].length();
+//     }
+// }
+
+// Other solution
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] st = s.split(" ");
-        return st[st.length-1].length();
+
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        int length = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 }
