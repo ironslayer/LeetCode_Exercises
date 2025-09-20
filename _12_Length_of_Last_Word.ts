37357d9bb218e1036849a6f5e1ae307973f72fd5
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-19 23:36:51 -0400

    Solution problem 12 in typescript

---
+++ _12_Length_of_Last_Word.ts
@@ -0,0 +1,20 @@
+function lengthOfLastWord(s: string): number {
+    let i = s.length - 1;
+
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+
+    let length = 0;
+
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+}
+
+console.log(lengthOfLastWord("Hello World"));               
+console.log(lengthOfLastWord("   fly me   to   the moon  ")); 
+console.log(lengthOfLastWord("luffy is still joyboy"));       
