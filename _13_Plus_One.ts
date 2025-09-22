8714a51d4a0c45ccd26d50f80a64791f260c0102
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-21 22:45:29 -0400

    Other solutiuon problem 13. i ntypescript

--- _13_Plus_One.ts
+++ _13_Plus_One.ts
@@ -1,17 +1,32 @@
+// function plusOne(digits: number[]): number[] {
+//     let n = digits.length;
+
+//     for (let i = n - 1; i >= 0; i--) {
+//         if (digits[i] < 9) {
+//             digits[i]++;
+//             return digits;
+//         }
+//         digits[i] = 0;
+//     }
+
+//     const result = new Array(n + 1).fill(0);
+//     result[0] = 1;
+//     return result;
+// }
+
+
+//Other soplution 
+
 function plusOne(digits: number[]): number[] {
-    let n = digits.length;
-
-    for (let i = n - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits;
-        }
-        digits[i] = 0;
+    const n = digits.length
+    let pos = n - 1
+    while (digits[pos] === 9) {
+        digits[pos] = 0
+        pos--
     }
-
-    const result = new Array(n + 1).fill(0);
-    result[0] = 1;
-    return result;
+    digits[pos]++
+    if (pos === -1) digits.unshift(1)
+    return digits
 }
 
 console.log(plusOne([1, 2, 3]));       
