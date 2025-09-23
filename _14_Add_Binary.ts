78017fc5d0412dd53e5dcd77e9fd9ed686fd670a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-22 23:20:54 -0400

    Other solution problme 14 en typescript

--- _14_Add_Binary.ts
+++ _14_Add_Binary.ts
@@ -1,34 +1,39 @@
+// function addBinary(a: string, b: string): string {
+//     if (b.length > a.length) {
+//         return addBinary(b, a);
+//     }
+
+//     const result: string[] = new Array(a.length + 1).fill("0");
+//     let sum = 0;
+//     let i = a.length - 1;
+//     let j = b.length - 1;
+//     let carry = 0;
+
+//     while (i >= 0) {
+//         sum = (parseInt(a[i]) || 0) + carry;
+
+//         if (j >= 0) {
+//             sum += parseInt(b[j]);
+//             j--;
+//         }
+
+//         carry = Math.floor(sum / 2);
+//         result[i + 1] = String(sum % 2);
+
+//         i--;
+//     }
+
+//     if (carry === 1) {
+//         result[0] = "1";
+//         return result.join("");
+//     }
+
+//     return result.slice(1).join(""); 
+// }
+
+// Other solution
 function addBinary(a: string, b: string): string {
-    if (b.length > a.length) {
-        return addBinary(b, a);
-    }
-
-    const result: string[] = new Array(a.length + 1).fill("0");
-    let sum = 0;
-    let i = a.length - 1;
-    let j = b.length - 1;
-    let carry = 0;
-
-    while (i >= 0) {
-        sum = (parseInt(a[i]) || 0) + carry;
-
-        if (j >= 0) {
-            sum += parseInt(b[j]);
-            j--;
-        }
-
-        carry = Math.floor(sum / 2);
-        result[i + 1] = String(sum % 2);
-
-        i--;
-    }
-
-    if (carry === 1) {
-        result[0] = "1";
-        return result.join("");
-    }
-
-    return result.slice(1).join(""); 
+    return (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
 }
 
 console.log(addBinary("11", "1"));        
