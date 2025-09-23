70ffebb6dbd093fa2538bf8290cfd0484dcab60c
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-23 04:50:57 -0400

    Solution problem 15 in typescript

---
+++ _15_Sqrt_x_ .ts
@@ -0,0 +1,9 @@
+function mySqrt(x: number): number {
+    const sqrt = Math.sqrt(x);
+    return Math.floor(sqrt);
+}
+
+console.log(mySqrt(8));  
+console.log(mySqrt(4));  
+console.log(mySqrt(16));  
+console.log(mySqrt(29));  
