419f42f90af06338691b40da9589db38ee7b3e43
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-24 02:52:40 -0400

    solution problem 16 in java

---
+++ _16_Climbing_Stairs.java
@@ -0,0 +1,44 @@
+public class _16_Climbing_Stairs {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.climbStairs(2));        
+        System.out.println(sol.climbStairs(3));  
+        System.out.println(sol.climbStairs(4));        
+        System.out.println(sol.climbStairs(5));     
+        System.out.println(sol.climbStairs(6));     
+        System.out.println(sol.climbStairs(7));     
+        System.out.println(sol.climbStairs(8));     
+        System.out.println(sol.climbStairs(9));     
+
+    }
+}
+
+class Solution {
+    public int climbStairs(int n) {
+
+        int a = 2;
+        int b = 3;
+        int ans=n;
+
+        n-=3;
+
+        while (n>0) {
+            ans = b+a;
+            a = b;
+            b = ans;
+            n--;
+        }
+        return ans;
+    }
+}
+
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+}
