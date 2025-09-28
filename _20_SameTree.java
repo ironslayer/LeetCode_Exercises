8d0c2dec9854da2ee277355fe76cd791d586faff
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-28 05:26:12 -0400

    Solution problem 20 in java

---
+++ _20_SameTree.java
@@ -0,0 +1,48 @@
+public class _20_SameTree {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        TreeNode p1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        System.out.println(sol.isSameTree(p1, q1)); 
+
+        TreeNode p2 = new TreeNode(1, new TreeNode(2), null);
+        TreeNode q2 = new TreeNode(1, null, new TreeNode(2));
+        System.out.println(sol.isSameTree(p2, q2)); 
+
+        TreeNode p3 = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+        TreeNode q3 = new TreeNode(1, new TreeNode(1), new TreeNode(2));
+        System.out.println(sol.isSameTree(p3, q3)); 
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
