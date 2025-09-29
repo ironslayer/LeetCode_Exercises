ef5bfe794253d7b2c93aeeecb00971db7bad7b2a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-29 05:14:51 -0400

    Other solution problem 21 in java

--- _21_Symmetric_Tree.java
+++ _21_Symmetric_Tree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class _21_Symmetric_Tree {
     public static void main(String[] args) {
         Solution sol = new Solution();
@@ -29,20 +32,47 @@ class TreeNode {
 }
 
 //My solution
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         return isMirror(root.left, root.right);
+//     }
+
+//     private boolean isMirror(TreeNode left, TreeNode right) {
+
+//         if (left == null && right == null) return true;
+        
+//         if (left == null || right == null) return false;
+
+//         if (left.val != right.val) return false;
+
+//         return isMirror( left.left, right.right ) && isMirror(left.right, right.left);
+//     }
+
+// }
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return isMirror(root.left, root.right);
+        if (root == null) return true;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+
+            queue.add(t1.left);
+            queue.add(t2.right);
+
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+
+        return true;
     }
-
-    private boolean isMirror(TreeNode left, TreeNode right) {
-
-        if (left == null && right == null) return true;
-        
-        if (left == null || right == null) return false;
-
-        if (left.val != right.val) return false;
-
-        return isMirror( left.left, right.right ) && isMirror(left.right, right.left);
-    }
-
 }
