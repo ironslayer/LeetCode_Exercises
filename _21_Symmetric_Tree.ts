d3dbd76df08467e537ab5791cbf430027620ec6f
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-09-29 05:24:54 -0400

    Solution problem 21 in typescript

---
+++ _21_Symmetric_Tree.ts
@@ -0,0 +1,53 @@
+class TreeNode3 {
+  val: number;
+  left: TreeNode3 | null;
+  right: TreeNode3 | null;
+
+  constructor(val?: number, left?: TreeNode3 | null, right?: TreeNode3 | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isSymmetric(root: TreeNode3 | null): boolean {
+  if (!root) return true;
+
+  function isMirror(left: TreeNode3 | null, right: TreeNode3 | null): boolean {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+    if (left.val !== right.val) return false;
+
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+  }
+
+  return isMirror(root.left, root.right);
+}
+
+function main() {
+  const root1 = new TreeNode3(
+    1,
+    new TreeNode3(2, new TreeNode3(3), new TreeNode3(4)),
+    new TreeNode3(2, new TreeNode3(4), new TreeNode3(3))
+  );
+  console.log("Ejemplo 1:", isSymmetric(root1));
+
+  const root2 = new TreeNode3(
+    1,
+    new TreeNode3(2, null, new TreeNode3(3)),
+    new TreeNode3(2, null, new TreeNode3(3))
+  );
+  console.log("Ejemplo 2:", isSymmetric(root2));
+
+  const root3 = new TreeNode3(1); 
+  console.log("Ejemplo 3:", isSymmetric(root3)); 
+
+  const root4 = new TreeNode3(
+    1,
+    new TreeNode3(2, new TreeNode3(3), null),
+    new TreeNode3(2, null, new TreeNode3(3))
+  );
+  console.log("Ejemplo 4:", isSymmetric(root4)); 
+}
+
+main();
