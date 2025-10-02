911a6a8d73ce53b1f4f07a1b6d7a02ca810a46d3
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-10-02 15:42:17 -0400

    Other solution in java problem 24

--- _24_Find_Center_of_Star_Graph.java
+++ _24_Find_Center_of_Star_Graph.java
@@ -3,34 +3,45 @@ public class _24_Find_Center_of_Star_Graph {
         Solution sol = new Solution();
 
         int[][] edges1 = { {1,2}, {2,3}, {4,2} };
-        System.out.println("Ejemplo 1: " + sol.findCenter(edges1)); // esperado: 2
+        System.out.println("Ejemplo 1: " + sol.findCenter(edges1)); 
 
         int[][] edges2 = { {1,2}, {5,1}, {1,3}, {1,4} };
-        System.out.println("Ejemplo 2: " + sol.findCenter(edges2)); // esperado: 1
+        System.out.println("Ejemplo 2: " + sol.findCenter(edges2)); 
 
         int[][] edges3 = { {7,6}, {2,7}, {3,7}, {4,7}, {5,7}, {1,7} };
-        System.out.println("Ejemplo 3: " + sol.findCenter(edges3)); // esperado: 7
+        System.out.println("Ejemplo 3: " + sol.findCenter(edges3)); 
     }
 }
 
+// class Solution {
+//     public int findCenter(int[][] edges) {
+//         int n = edges.length + 1; 
+//         int[] freq = new int[n + 1];
+
+//         for (int[] edge : edges) {
+//             int u = edge[0];
+//             int v = edge[1];
+//             freq[u]++;
+//             freq[v]++;
+//         }
+
+//         for (int i = 1; i <= n; i++) {
+//             if (freq[i] == n - 1) {
+//                 return i; 
+//             }
+//         }
+
+//         return -1; 
+//     }
+// }
+
+//Other solution
 class Solution {
     public int findCenter(int[][] edges) {
-        int n = edges.length + 1; 
-        int[] freq = new int[n + 1];
+        int a = edges[0][0], b = edges[0][1];
+        int c = edges[1][0], d = edges[1][1];
 
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            freq[u]++;
-            freq[v]++;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (freq[i] == n - 1) {
-                return i; 
-            }
-        }
-
-        return -1; 
+        if (a == c || a == d) return a;
+        return b;
     }
 }
