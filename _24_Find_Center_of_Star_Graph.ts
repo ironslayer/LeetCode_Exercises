4646ef6734f423f292f815b20505ad732cece9eb
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-10-02 15:46:04 -0400

    Other solution problem 24 in typescript

--- _24_Find_Center_of_Star_Graph.ts
+++ _24_Find_Center_of_Star_Graph.ts
@@ -1,8 +1,19 @@
+// function findCenter(edges: number[][]): number {
+//     return (edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1])
+//         ? edges[0][0]
+//         : edges[0][1];
+// }
+
+
+//Other solution
 function findCenter(edges: number[][]): number {
-    return (edges[0][0] === edges[1][0] || edges[0][0] === edges[1][1])
-        ? edges[0][0]
-        : edges[0][1];
-}
+  for (let i: number = 0; i < edges[0].length; i++) {
+    if (edges[1].includes(edges[0][i])) {
+      return edges[0][i]
+    }
+  };
+  return -1
+};
 
 
 function main() {
