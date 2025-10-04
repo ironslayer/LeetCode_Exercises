69f92f3a48948b49c3ce41835d0d425daf6ed6c5
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-10-03 22:58:50 -0400

    Solution problem 25 in java

---
+++ _25_Find_if_Path_Exists_in_Graph.java
@@ -0,0 +1,75 @@
+public class _25_Find_if_Path_Exists_in_Graph {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int n1 = 3;
+        int[][] edges1 = { {0,1}, {1,2}, {2,0} };
+        int source1 = 0, destination1 = 2;
+        System.out.println("Ejemplo 1: " + sol.validPath(n1, edges1, source1, destination1)); 
+
+        int n2 = 6;
+        int[][] edges2 = { {0,1}, {0,2}, {3,5}, {5,4}, {4,3} };
+        int source2 = 0, destination2 = 5;
+        System.out.println("Ejemplo 2: " + sol.validPath(n2, edges2, source2, destination2)); 
+
+        int n3 = 5;
+        int[][] edges3 = { {0,4}, {2,4}, {3,4}, {2,3} };
+        int source3 = 0, destination3 = 3;
+        System.out.println("Ejemplo 3: " + sol.validPath(n3, edges3, source3, destination3)); 
+    }
+}
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        UnionFind uf = new UnionFind(n);
+
+        // Unimos cada par de nodos conectados
+        for (int[] edge : edges) {
+            uf.union(edge[0], edge[1]);
+        }
+
+        // Si source y destination están en el mismo conjunto → hay camino
+        return uf.find(source) == uf.find(destination);
+    }
+}
+
+// -----------------
+// Clase Union-Find
+// -----------------
+class UnionFind {
+    private int[] parent;
+    private int[] rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i; // cada nodo es su propio padre
+            rank[i] = 1;   // rango inicial
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // compresión de caminos
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            // unir por rango (el más alto absorbe al más bajo)
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
